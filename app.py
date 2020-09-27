7b2a93196ea385bf75796af81f8cae4dcaffc9e6
de_much de_much <demuch17@gmail.com>
2020-09-27 17:51:10 +0300

    Added all the files

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from db import db
+
+from security import authenticate, identity
+from resources.item import Item, ItemList
+from resources.user import UserRegister
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/de_mu/Desktop/Python/Section 6/data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'Denis'  # Not gonna be showing to others
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+db.init_app(app)
+
+# JWT creates new end point /auth when auth called we send username and password after than the user found /auth endpoint sends back JWT token
+jwt = JWT(app, authenticate, identity)
+                                       # and then we send the token with every API call and the server calls the identity func
+
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+
---
+++ resources/user.py
@@ -0,0 +1,32 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+
+
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type=str,
+        required=True,
+        help='The username must be provided!'
+        )
+    parser.add_argument('password',
+        type=str,
+        required=True,
+        help='The password must be provided!'
+        )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "The username is already exists"}, 400
+        else:
+            user = UserModel(data['username'], data['password'])
+            user.save_to_db()       
+
+        return {'message': 'User created successfully!'}, 201
+
